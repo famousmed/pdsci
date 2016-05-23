8f2077ea4d453fb933a537bdab89034709f08610
famousmed <51555945@qq.com>
2016-05-23 15:27:54 +0800

    1

--- src/com/pinde/sci/ctrl/login/LicenseController.java
+++ src/com/pinde/sci/ctrl/login/LicenseController.java
@@ -1,36 +1,36 @@
 package com.pinde.sci.ctrl.login;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-
+import com.pinde.core.license.PdLicense;
 import com.pinde.sci.common.GeneralController;
 import com.pinde.sci.common.GeneralMethod;
 import com.pinde.sci.common.GlobalConstant;
 import com.pinde.sci.common.InitConfig;
 import com.pinde.sci.dao.base.SysCfgMapper;
 import com.pinde.sci.model.mo.SysCfg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LicenseController extends GeneralController {
 
 	private static Logger logger = LoggerFactory.getLogger(LicenseController.class);
 	
-	@Resource
+	@Autowired
 	private SysCfgMapper sysCfgMapper;
 	
 	/**
 	 * 显示授权
 	 * @return
 	 */
-	@RequestMapping(value = { "/license","lic" }, method = RequestMethod.GET)
+	@RequestMapping(value = { "/license" }, method = RequestMethod.GET)
 	public String license(HttpServletRequest request) {
 		return "license";
 	}
@@ -39,17 +39,31 @@ public class LicenseController extends GeneralController {
 	 * 显示授权
 	 * @return
 	 */
-	@RequestMapping(value = { "/license/upload" }, method = RequestMethod.POST)
+	@RequestMapping(value = { "lic" }, method = RequestMethod.GET)
+	public String lic(HttpServletRequest request) {
+		return "lic";
+	}
+	
+	/**
+	 * 显示授权
+	 * @return
+	 */
+	@RequestMapping(value = { "/lic/upload" }, method = RequestMethod.POST)
 	@ResponseBody
 	public String upload(MultipartFile licenseFile,HttpServletRequest request) {
 		if(!"license.key".equals(licenseFile.getOriginalFilename())){
-			return GlobalConstant.SAVE_FAIL;
+			return GlobalConstant.SAVE_FAIL+":文件名不对！";
 		}
 		String content = "";
 		try {
 			content = new String(licenseFile.getBytes(),"GBK");
+			boolean check = PdLicense.checkLicense(content);
+			if(check==false){
+				return GlobalConstant.SAVE_FAIL+":不是有效的license.key！";
+			}
 		} catch (Exception e) {
 			logger.error("license内容有误",e);
+			return GlobalConstant.SAVE_FAIL+":不是有效的license.key！";
 		} 
 		SysCfg cfg = cfgBiz.read("license.key");
 		if (cfg == null) {
